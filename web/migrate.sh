e9522a09d8331d9289f01c9d8efd93e4ac77549e
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-06-16 13:43:36 -0400

    allowed host fix try

--- web/migrate.sh
+++ web/migrate.sh
@@ -2,7 +2,9 @@
 DJANGO_SUERPUSER_EMAIL=${DJANGO_SUERPUSER_EMAIL:-"jonmcgaha@icloud.com"}
 cd /app/
 /opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $DJANGO_SUERPUSER_EMAIL --noinput || true
+
+
+# /opt/venv/bin/python manage.py createsuperuser --email $DJANGO_SUERPUSER_EMAIL --noinput || true
 
 # If you have email setup in Django, uncomment the following: 
 # /opt/venv/bin/python manage.py sendtestemail --admins
