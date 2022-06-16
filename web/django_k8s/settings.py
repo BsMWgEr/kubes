b3ff4e11beafe567659ae07d6719f6b4a2204746
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-06-16 17:45:57 -0400

    allowed host fix try

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jsquad.dev']
 
 
 # Application definition
