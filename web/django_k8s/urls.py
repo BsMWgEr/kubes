ac0bf1566af8390180e7a1fb1582cdbc453ff0c9
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-06-18 17:26:16 -0400

    test

--- web/django_k8s/urls.py
+++ web/django_k8s/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from views import index
+from . import views
 
 urlpatterns = [
-    path('/', index),
+    path('/', views.index),
     path('admin/', admin.site.urls),
 ]
