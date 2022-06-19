750016555fb31b372b6b8a263c5fa89b77b1e811
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-06-18 20:03:05 -0400

    test

--- web/migrate.sh
+++ web/migrate.sh
@@ -2,7 +2,7 @@
 DJANGO_SUPERUSER_EMAIL=${DJANGO_SUERPUSER_EMAIL:-"jonmcgaha@icloud.com"}
 cd /app/
 /opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --noinput || true
+
 
 # If you have email setup in Django, uncomment the following: 
 # /opt/venv/bin/python manage.py sendtestemail --admins
