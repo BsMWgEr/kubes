fd019e3dc4747c9d2c5697b89d38ec45831a7e09
Jonathan McGaha <87310106+BsMWgEr@users.noreply.github.com>
2022-07-29 11:56:15 -0400

    newcommit

--- web/migrate.sh
+++ web/migrate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 DJANGO_SUPERUSER_EMAIL=${DJANGO_SUERPUSER_EMAIL:-"jonmcgaha@icloud.com"}
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUERPUSER_USERNAME:-"jonmcgaha"}
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUERPUSER_PASSWORD:-"jonmcgaha@icloud.com"}
 cd /app/
 /opt/venv/bin/python manage.py migrate --noinput
+/opt/venv/bin/python manage.py createsuperuser --username $DJANGO_SUPERUSER_USERNAME --password $DJANGO_SUERPUSER_PASSWORD  --email $SUPERUSER_EMAIL --noinput || true
 
 
 # If you have email setup in Django, uncomment the following: 
